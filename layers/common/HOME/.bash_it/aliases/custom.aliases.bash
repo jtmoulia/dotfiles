bb1d6dee7a9ff91297dca1e7ff19de1b03d58374
Thomas Moulia <jtmoulia@gmail.com>
2017-10-26 11:34:01 -0700

    Add emacs daemon systemd service and emacsclient cmds

--- layers/common/HOME/.bash_it/aliases/custom.aliases.bash
+++ layers/common/HOME/.bash_it/aliases/custom.aliases.bash
@@ -2,3 +2,4 @@ alias spotify='spotify --force-device-scale-factor=1.8'
 alias chrome='chromium --force-device-scale-factor=1.8'
 alias vim=nvim
 alias rm='echo "rm disabled. try trash-rm or \rm to force."; false'
+alias ec='emacsclient'
