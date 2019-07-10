def2fca61567966293dad60b00db08f46884ec7a
Thomas Moulia <thomas@healthtensor.com>
2019-07-10 13:56:17 -0700

    Fix ansible vault hooks, remove redundant ivy conf

--- playbooks/roles/emacs/files/spacemacs.d/config/ansible.el
+++ playbooks/roles/emacs/files/spacemacs.d/config/ansible.el
@@ -1,16 +1,16 @@
 (defvar personal-ansible--vault-password-files
   `(
-    (,(expand-file-name "~/repos/automed/hippos/roles/automed/files/Prod.yml")
+    (,(intern (expand-file-name "~/repos/automed/hippos/roles/automed/files/Prod.yml"))
      . ,(expand-file-name "~/repos/automed/hippos/Prod-password"))
-    (,(expand-file-name "~/repos/automed/hippos/roles/automed/files/test.yml")
-     . ,(expand-file-name "~/repos/automed/hippos/test-password"))
+    (,(intern (expand-file-name "~/repos/automed/hippos/roles/automed/files/test.yml"))
+     . ,(expand-file-name "~/repos/automed/hippos/Test-password"))
     )
-  "Personal ansible vault password files.")
+  "alist of personal ansible vault password files where the keys are symbols")
 
 (defun personal-ansible//set-ansible-vault-password-file ()
   "Set the ansible vault password file using the buffer file
 to look up the password file from `personal-ansible--vault-password-files'"
-  (if-let ((password-file (alist-get buffer-file-name personal-ansible--vault-password-files)))
-      (set-variable 'ansible-vault-password-file) password-file))
+  (if-let ((password-file (alist-get (intern buffer-file-name) personal-ansible--vault-password-files)))
+      (set-variable 'ansible-vault-password-file password-file)))
 
 (add-hook 'yaml-mode-hook 'personal-ansible//set-ansible-vault-password-file)
