5fd33c0cf7d350c1da28a2ebaa0278fc0788e74d
Thomas Moulia <jtmoulia@gmail.com>
2020-07-24 00:45:28 -0700

    Fix up stale offlineimap config

--- playbooks/roles/offlineimap/files/offlineimap.py
+++ playbooks/roles/offlineimap/files/offlineimap.py
@@ -1,11 +1,10 @@
 import subprocess
-from typing import Optional
 
 
-DEFAULT_PASS_GET = "/home/jtmoulia/bin/pass-get"
+DEFAULT_PASS_GET = "/home/jtmoulia/.local/bin/pass-get"
 
 
-def pass_get(pass_name: str, key: str) -> Optional[str]:
+def pass_get(pass_name, key):
     """Use ``pass`` to get a particular line from a password file.
 
     See ``pass-get``.
