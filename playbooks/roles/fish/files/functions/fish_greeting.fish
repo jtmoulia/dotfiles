6c832c65ac10b2300ec458c588a5081acc7abc87
Thomas Moulia <jtmoulia@gmail.com>
2020-07-23 23:06:46 -0700

    Remove unused layers, do some quoting

--- playbooks/roles/fish/files/functions/fish_greeting.fish
+++
@@ -1,3 +0,0 @@
-function fish_greeting --description "say hi fish"
-  fortune
-end
--- playbooks/roles/fish/files/set_initial_globals.fish
+++
@@ -1,6 +0,0 @@
-set -U EDITOR nvim
-set -U GDK_SCALE 1
-
-if [ ! (contains $PATH ~/bin) ]
-    set -U fish_user_paths ~/bin $fish_user_paths
-end
