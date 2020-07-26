fbb0d337b6a33787d8f05186088c989b777a8830
Thomas Moulia <jtmoulia@gmail.com>
2020-07-26 00:39:47 -0700

    Improve conky config and symlink it in

--- playbooks/roles/conky/files/conky.conf.lua
+++ playbooks/roles/conky/files/conky.conf.lua
@@ -25,21 +25,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]
 
 conky.config = {
-    alignment = 'bottom_right',
+    alignment = 'top_right',
     background = false,
     border_width = 1,
     cpu_avg_samples = 2,
     default_color = 'white',
     default_outline_color = 'white',
-    default_shade_color = 'white',
-    draw_borders = false,
+    default_shade_color = 'black',
+    draw_borders = true,
     draw_graph_borders = true,
     draw_outline = false,
-    draw_shades = false,
+    draw_shades = true,
+    double_buffer = true,
     use_xft = true,
-    font = 'DejaVu Sans Mono:size=12',
-    gap_x = 5,
-    gap_y = 60,
+    font = 'DejaVu Sans Mono:size=14',
+    gap_x = 12,
+    gap_y = 48,
     minimum_height = 5,
     minimum_width = 5,
     net_avg_samples = 2,
@@ -49,9 +50,12 @@ conky.config = {
     out_to_stderr = false,
     out_to_x = true,
     extra_newline = false,
-    own_window = true,
     own_window_class = 'Conky',
-    own_window_type = 'desktop',
+    own_window_type = 'normal',
+    own_window_transparent = true,
+    own_window_argb_visual = true,
+    own_window_argb_value = 0,
+    own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
     stippled_borders = 0,
     update_interval = 1.0,
     uppercase = false,
@@ -61,25 +65,40 @@ conky.config = {
 }
 
 conky.text = [[
-${scroll 16 $nodename - $sysname $kernel on $machine | }
+${color grey}System:$color $alignr $sysname $kernel $machine
+${color grey}Account:$color $alignr ${execi 100000 whoami}@$nodename
+${color grey}Time:$color $alignr $time
+${color grey}Uptime:$color $alignr $uptime
 $hr
-${color grey}Uptime:$color $uptime
-${color grey}Frequency (in MHz):$color $freq
-${color grey}Frequency (in GHz):$color $freq_g
-${color grey}RAM Usage:$color $mem/$memmax - $memperc% ${membar 4}
-${color grey}Swap Usage:$color $swap/$swapmax - $swapperc% ${swapbar 4}
+${color grey}RAM Usage:$color $alignr $mem/$memmax $memperc%
+  ${membar 4}
+${color grey}Swap Usage:$color $alignr $swap/$swapmax - $swapperc%
+  ${swapbar 4}
+$hr
+${color grey}CPU Frequency (in GHz):$color $alignr $freq_g
 ${color grey}CPU Usage:$color $cpu% ${cpubar 4}
+ ${color grey}CPU 1:$color ${cpu cpu1}% ${cpubar cpu1 4}
+ ${color grey}CPU 2:$color ${cpu cpu2}% ${cpubar cpu2 4}
+ ${color grey}CPU 3:$color ${cpu cpu3}% ${cpubar cpu3 4}
+ ${color grey}CPU 4:$color ${cpu cpu4}% ${cpubar cpu4 4}
+ ${cpugraph}
 ${color grey}Processes:$color $processes  ${color grey}Running:$color $running_processes
 $hr
 ${color grey}File systems:
  / $color${fs_used /}/${fs_size /} ${fs_bar 6 /}
 ${color grey}Networking:
-Up:$color ${upspeed} ${color grey} - Down:$color ${downspeed}
+ ${color grey}IP Address (wlp3s0):$color $alignr ${execi 600 ip addr show wlp3s0 | perl -ne 'print "$1\n" if /inet (.*)\//'}
+ ${color grey}Up / Down:$color ${upspeed wlp3s0} $alignr ${downspeed wlp3s0}
+ ${upspeedgraph wlp3s0}
+ ${downspeedgraph wlp3s0}
+${color grey}Battery:$color ${battery_short BAT1} ${battery_bar 4 BAT1}
 $hr
-${color grey}Name              PID   CPU%   MEM%
+${color grey}Process Name     PID    CPU%   MEM%
 ${color lightgrey} ${top name 1} ${top pid 1} ${top cpu 1} ${top mem 1}
 ${color lightgrey} ${top name 2} ${top pid 2} ${top cpu 2} ${top mem 2}
 ${color lightgrey} ${top name 3} ${top pid 3} ${top cpu 3} ${top mem 3}
 ${color lightgrey} ${top name 4} ${top pid 4} ${top cpu 4} ${top mem 4}
 ${color lightgrey} ${top name 5} ${top pid 5} ${top cpu 5} ${top mem 5}
+${color grey}Failed systemd units:
+${color lightgrey}${execi 300 systemctl list-units --failed --full --all --plain --no-legend | perl -ne 'print " $1\n" if /^(.*) +loaded.*$/'}
 ]]
