200381be03cc7de2f0e3302f712e3788c3672bc0
Thomas Moulia <jtmoulia@gmail.com>
2022-06-28 16:37:22 -0700

    Improve awslogs helper script

--- playbooks/roles/automed/files/alog.bash
+++ playbooks/roles/automed/files/alog.bash
@@ -2,7 +2,7 @@
 
 if [ $# -eq 0 ]; then
     echo "Missing log group argument (e.g. SLHSProd)"
-    echo "Usage: alog GROUP [STREAM] [SINCE]"
+    echo "Usage: alog GROUP [STREAM] [SINCE] [FILTER]"
     exit 1
 elif [ $# -eq 1 ]; then
     aws logs tail "$1-logs" --follow
@@ -10,4 +10,6 @@ elif [ $# -eq 2 ]; then
     aws logs tail "$1-logs" --log-stream-names "$2" --follow
 elif [ $# -eq 3 ]; then
     aws logs tail "$1-logs" --log-stream-names "$2" --since "$3" --follow
+elif [ $# -eq 4 ]; then
+    aws logs tail "$1-logs" --log-stream-names "$2" --since "$3" --filter-pattern "$4" --follow
 fi
