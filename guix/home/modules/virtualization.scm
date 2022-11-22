cff7fb0bcc541aae5e4a26c15eba73e4086ec813
Thomas Moulia <jtmoulia@gmail.com>
2022-11-21 22:19:30 -0800

    Begin adding virtualization home config

---
+++ guix/home/modules/virtualization.scm
@@ -0,0 +1,15 @@
+(define-module (home modules virtualization)
+  #:use-module (gnu services)
+  #:use-module (gnu services virtualization)
+  )
+
+(define-public virtualization-packages
+  (map specification->package
+       (list
+        "libvirt"
+        )))
+
+(define-public virtualization-services
+  (list
+   (service libvirt-service-type
+            (libvirt-configuration))))
