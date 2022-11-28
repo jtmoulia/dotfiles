be7d8d7f239d4faedce2c9fd4b8ab5fed6a55220
Thomas Moulia <jtmoulia@gmail.com>
2022-11-28 14:40:20 -0800

    Add concept of dotfiles env var as stopgap

--- guix/home/files/scripts/reconfigure-system.sh
+++ guix/home/files/scripts/reconfigure-system.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-GUIX_SYSTEM_CONFIG="/etc/config.scm"
-GUIX_SYSTEM_LOAD_PATH="$HOME/.dotfiles/guix"
+DOTFILES="${DOTFILES:-"$HOME/.dotfiles"}"
+GUIX_SYSTEM_CONFIG="${GUIX_SYSTEM_CONFIG:-"$DOTFILES/guix/system0/config.scm"}"
+GUIX_SYSTEM_LOAD_PATH="${GUIX_SYSTEM_LOAD_PATH:-"$DOTFILES/guix"}"
 sudo guix system reconfigure \
     --load-path="$GUIX_SYSTEM_LOAD_PATH" \
     $@ "$GUIX_SYSTEM_CONFIG"
