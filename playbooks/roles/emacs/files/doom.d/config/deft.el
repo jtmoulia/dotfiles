faf5786fe40b8568060cf6b8c4d752ae77c75b97
Thomas Moulia <jtmoulia@gmail.com>
2022-12-16 23:45:36 -0800

    Use org-directory for deft notes base dir

--- playbooks/roles/emacs/files/doom.d/config/deft.el
+++ playbooks/roles/emacs/files/doom.d/config/deft.el
@@ -1,7 +1,9 @@
 ;;; doom.d/config/deft.el -*- lexical-binding: t; -*-
 
+(require 'f)
+
 ;; Assumes that nextcloud is mounted to ~/ocloud and org/ is sync'd
-(setq deft-directory "~/ocloud/org/notes")
+(setq deft-directory (f-join org-directory "notes"))
 
 ;; Search down in deft sub-directories
 (setq deft-recursive t)
