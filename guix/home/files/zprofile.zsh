6ef85eddf59dc429ebdec398dc7cd5ea8d713394
Thomas Moulia <jtmoulia@gmail.com>
2022-12-25 22:19:29 -0800

    Add ~/.local/bin to the PATH

--- guix/home/files/zprofile.zsh
+++ guix/home/files/zprofile.zsh
@@ -3,6 +3,9 @@
 GUIX_PROFILE="${GUIX_PROFILE:-"$HOME/.guix-profile"}"
 source "$GUIX_PROFILE/etc/profile"
 
+# Add my local dir to the path
+PATH="~/.local/bin:$PATH"
+
 # Get flatpak XDG paths added
 if [ -f "$GUIX_PROFILE/etc/profile.d/flatpak.sh" ]; then
     source "$GUIX_PROFILE/etc/profile.d/flatpak.sh"
