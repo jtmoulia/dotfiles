0a5a537d22bfda8bdfc124f49d40f81f9f347485
Thomas Moulia <jtmoulia@gmail.com>
2023-03-10 20:31:26 -0800

    Explicitly import and specify waybar package

--- guix/home/modules/wm.scm
+++ guix/home/modules/wm.scm
@@ -1,51 +1,54 @@
 (define-module (home modules wm)
   #:use-module (guix gexp)
   #:use-module (gnu packages)
+  #:use-module (gnu packages wm)
   #:use-module (gnu services)
   #:use-module (gnu home services))
 
 ;; NOTE: foot is configured along with sway for simplicity's sake.
 
 (define-public sway-packages
-  (map specification->package
-       (list
-        ;; TODO: alacritty hasn't been working, drop or fix
-        "alacritty"
-        ;; CLI utility to control screen brightness
-        "brightnessctl"
-        ;; a simple shell (because alacritty isn't working?!)
-        "foot"
-        ;; emoji friendly noto font: =Noto Emoji [Weight]=
-        "font-noto-emoji"
-        ;; grimshot for taking screenshots of windows / full screen
-        "grimshot"
-        ;; desktop notification plumbing
-        "libnotify"
-        ;; wayland desktop notification presentation
-        ;; NOTE: mako build currently broken
-        "mako"
-        ;; sway window manager (@ddevault)
-        ;; https://github.com/swaywm/sway
-        "sway"
-        ;; idle management daemon for Wayland compositors
-        "swayidle"
-        ;; Swaylock for sway screen locking
-        ;; NOTE: Installed by the system profile for setuid privs
-        ;; https://github.com/swaywm/swaylock
-        ;; "swaylock"
-        ;; sway window LRU, gathered by separate running server
-        ;; https://sr.ht/~tsdh/swayr/
-        "swayr"
-        ;; waybar for a wayland system bar; configured with sway
-        "waybar"
-        ;; an application for reading inputs within wayland
-        "wev"
-        ;; screen recorder for wayland
-        "wf-recorder"
-        ;; wayland friendly clipboard (alias `xc` from shell)
-        "wl-clipboard"
-        ;; launcher, wofi style
-        "wofi")))
+  (cons
+   ;; waybar for a wayland system bar; configured with sway
+   ;; Note: specification->package can't be used as there are multiple waybar defs
+   waybar
+   (map specification->package
+        (list
+         ;; TODO: alacritty hasn't been working, drop or fix
+         "alacritty"
+         ;; CLI utility to control screen brightness
+         "brightnessctl"
+         ;; a simple shell (because alacritty isn't working?!)
+         "foot"
+         ;; emoji friendly noto font: =Noto Emoji [Weight]=
+         "font-noto-emoji"
+         ;; grimshot for taking screenshots of windows / full screen
+         "grimshot"
+         ;; desktop notification plumbing
+         "libnotify"
+         ;; wayland desktop notification presentation
+         ;; NOTE: mako build currently broken
+         "mako"
+         ;; sway window manager (@ddevault)
+         ;; https://github.com/swaywm/sway
+         "sway"
+         ;; idle management daemon for Wayland compositors
+         "swayidle"
+         ;; Swaylock for sway screen locking
+         ;; NOTE: Installed by the system profile for setuid privs
+         ;; https://github.com/swaywm/swaylock
+         ;; "swaylock"
+         ;; sway window LRU, gathered by separate running server
+         ;; https://sr.ht/~tsdh/swayr/
+         "swayr"
+         ;; an application for reading inputs within wayland
+         "wev"
+         ;; screen recorder for wayland
+         "wf-recorder"
+         ;; wayland friendly clipboard (alias `xc` from shell)
+         "wl-clipboard"
+         ;; launcher, wofi style
+         "wofi"))))
 
 (define-public sway-services
   (list
