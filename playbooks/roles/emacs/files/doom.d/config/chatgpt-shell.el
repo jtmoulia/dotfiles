67e8190bc74b209e2b58577d4b879e8456561778
Thomas Moulia <jtmoulia@gmail.com>
2023-06-25 21:27:34 -0700

    Emacs: update chatgpt-shell org / shell prompting

--- playbooks/roles/emacs/files/doom.d/config/chatgpt-shell.el
+++ playbooks/roles/emacs/files/doom.d/config/chatgpt-shell.el
@@ -1,4 +1,17 @@
 ;;; ../.dotfiles/playbooks/roles/emacs/files/doom.d/config/chatgpt-shell.el -*- lexical-binding: t; -*-
+(require 'a)
+
+;; HACK: for some reason straight build doesn't include ob-chatgpt-shell. So,
+;; instead we add the repo dir to the load-path :shrug:
+(add-to-list 'load-path "~/.emacs.d/.local/straight/repos/chatgpt-shell")
+(require 'ob-chatgpt-shell)
+(ob-chatgpt-shell-setup)
+
+;; The "Prorg" prompt just uses the "Programming" prompt with org-mode formatting
+(add-to-list 'chatgpt-shell-system-prompts
+             `("Prorg" . ,(string-replace
+                           "markdown" "org-mode markup"
+                           (a-get chatgpt-shell-system-prompts "Programming"))))
 
 ;; Load OpenAI API key from .authinfo.gpg
 (setq chatgpt-shell-openai-key
@@ -9,15 +22,13 @@
 ;; NOTE: function calling isn't supported by chatgpt-shell (yet)
 (setq chatgpt-shell-model-version "gpt-4-0613")
 
-;; HACK: for some reason straight build doesn't include ob-chatgpt-shell. So,
-;; instead we add the repo dir to the load-path :shrug:
-(add-to-list 'load-path "~/.emacs.d/.local/straight/repos/chatgpt-shell")
-(require 'ob-chatgpt-shell)
-(ob-chatgpt-shell-setup)
+;; Use the programming prompt in the shell as it plays well with the formatting
+(setq chatgpt-shell-system-prompt
+      (cl-position "Programming" chatgpt-shell-system-prompts :key #'car :test #'equal))
 
+;; Use the "Prorg" prompt for org-mode source blocks
 (setq org-babel-default-header-args:chatgpt-shell
-      `((:results . "code")
+      `((:results . "raw")
         (:version . "gpt-4-0613")
-        (:system . ,(a-get chatgpt-shell-system-prompts "Programming"))
-        (:temperature . 0)
-        (:wrap . "EXPORT markdown")))
+        (:system . ,(a-get chatgpt-shell-system-prompts "Prorg"))
+        (:temperature . 0)))
