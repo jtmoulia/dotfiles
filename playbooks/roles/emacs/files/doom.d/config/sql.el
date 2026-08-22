fd9917f6001e389268e7e31b50958bff1eb5a816
Thomas Moulia <jtmoulia@gmail.com>
2026-08-21 21:08:12 -0700

    emacs: replace doom with hogmacs literate config
    Drop doom-emacs (git clone into ~/.emacs.d, ~/.doom.d symlink) along
    with all doom.d config/snippets, in favor of cloning the hogmacs
    literate config (git.sr.ht/~jtmoulia/hogmacs) into ~/.config/emacs
    and tangling README.org to init.el.
    
    Move the personal aspell dictionary out of doom.d and symlink it to
    ~/.config/aspell/aspell.en.pws, where hogmacs' flyspell config
    expects it. Drop the stale, unused ispell.pws placeholder and the
    emacs_mu4e_enabled/emacs_org_jira_enabled/emacs_org_page_enabled/
    emacs_prodigy_enabled vars, which drove doom.d/config.el and have no
    consumer now.

--- playbooks/roles/emacs/files/doom.d/config/sql.el
+++
@@ -1,6 +0,0 @@
-;;; ~/.dotfiles/playbooks/roles/emacs/files/doom.d/config/sql.el -*- lexical-binding: t; -*-
-
-;; Save the session interaction
-(setq sql-input-ring-file-name "~/.emacs.d/var/sql-history.log")
-
-;;; sql.el ends here
